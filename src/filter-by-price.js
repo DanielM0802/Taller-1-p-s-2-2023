794a4ec772198dc916dd62b1977cfd54e06ba731
Elian Bustamante <e.bustamante02@ufromail.cl>
2023-09-05 20:18:45 -0300

    Se realiza documentación de métodos de filter-by-price.js y de utils.js

--- src/filter-by-price.js
+++ src/filter-by-price.js
@@ -3,6 +3,13 @@ import utils from "./utils/utils.js";
 
 const { generateXlsxFile, generateJsonFile } = utils;
 
+/**
+ * Filtra las casas por precio máximo y genera archivos XLSX y JSON con las casas filtradas.
+ * @param {Array} options.houses - Las casas a filtrar.
+ * @param {number} options.maximumPrice - El precio máximo permitido para las casas.
+ * @param {string} options.city - La ciudad de las casas.
+ * @returns {void}
+ */
 function filterByPrice({ houses, maximumPrice, city }) {
 	const filteredHouses = houses
 		.filter((house) => isHousePriceBelowMaximum(house, maximumPrice))
@@ -12,11 +19,22 @@ function filterByPrice({ houses, maximumPrice, city }) {
 	generateJsonFile(filteredHouses, city);
 }
 
+/**
+ * Comprueba si el precio de una casa es menor que el precio máximo dado.
+ * @param {Object} house - La casa a comprobar.
+ * @param {number} maximumPrice - El precio máximo permitido.
+ * @returns {boolean} - Verdadero si el precio de la casa es menor que el precio máximo, falso en caso contrario.
+ */
 function isHousePriceBelowMaximum(house, maximumPrice) {
 	const housePrice = Number(house.priceInCLP.replace("$", "").replace(/\./g, ""));
 	return housePrice < maximumPrice;
 }
 
+/**
+ * Devuelve un objeto con la ubicación y la URL de una casa.
+ * @param {Object} house - El objeto de la casa del que se extraerá la ubicación y la URL.
+ * @returns {Object} Un objeto con la ubicación y la URL de la casa.
+ */
 function getHouseLocationAndUrl(house) {
 	return {
 		Location: house.location,
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,16 +1,35 @@
 import XLSX from "xlsx";
 import fs from "fs";
 
+/**
+ * Genera un archivo XLSX con los datos de las casas y lo guarda en la ubicación especificada.
+ * @param {Array} houses - Un array de objetos que contienen los datos de las casas.
+ * @param {string} city - El nombre de la ciudad donde se encuentran las casas.
+ */
 function generateXlsxFile(houses, city) {
+	// Crear una hoja de trabajo a partir de los datos de las casas.
 	const workSheet = XLSX.utils.json_to_sheet(houses);
+
+	// Crear un libro de trabajo y agregar la hoja de trabajo.
 	const workBook = XLSX.utils.book_new();
+
 	XLSX.utils.book_append_sheet(workBook, workSheet, "Houses");
-	XLSX.writeFile(workBook, `./xlsx/${city}.xlsx`);
+
+	const filePath = `./xlsx/${city}.xlsx`;
+	XLSX.writeFile(workBook, filePath);
 	console.log(`${city} XLSX File generated successfully`);
 }
 
+/**
+ * Genera un archivo JSON con los datos de las casas y el nombre de la ciudad.
+ * @param {Array} houses - Un array de objetos que contienen los datos de las casas.
+ * @param {string} city - El nombre de la ciudad donde se encuentran las casas.
+ */
 function generateJsonFile(houses, city) {
-	fs.writeFile(`./json/${city}.json`, JSON.stringify(houses), (err) => {
+	const filePath = `./json/${city}.json`;
+	const jsonData = JSON.stringify(houses);
+
+	fs.writeFile(filePath, jsonData, (err) => {
 		if (err) {
 			console.log(err);
 		}
